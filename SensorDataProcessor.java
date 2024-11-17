23e92087d7264abb4cf2352e10d43401ad20e559
Razan Ibrahim <2111213@uj.edu.sa>
2024-11-17 20:48:53 +0300

    Updated SensorDataProcessor.java with necessary changes

--- SensorDataProcessor.java
+++ SensorDataProcessor.java
@@ -5,14 +5,14 @@ public class SensorDataProcessor {
     public double[][] limit; 
  
     // constructor 
-    public DataProcessor(double[][][] data, double[][] limit) { 
+    public SensorDataProcessor(double[][][] data, double[][] limit) { //Fixed be Razan Ibrahim - 2111213 : change constructor to match class name
         this.data = data; 
         this.limit = limit; 
     } 
  
     // calculates average of sensor data 
     private double average(double[] array) { 
-        int i = 0; 
+        int i=0; 
         double val = 0; 
         for (i = 0; i < array.length; i++) { 
             val += array[i]; 
@@ -24,7 +24,8 @@ public class SensorDataProcessor {
     // calculate data 
     public void calculate(double d) { 
  
-        int i, j, k = 0; 
+        int i, j;  //Fixed be Razan Ibrahim - 2111213 : change the variable declaration to initialize k separately
+        int k = 0;  //Fixed be Razan Ibrahim - 2111213
         double[][][] data2 = new 
 double[data.length][data[0].length][data[0][0].length]; 
  
@@ -35,18 +36,43 @@ Department of Software Engineering
  
  
         // Write racing stats data into a file 
-        private void writeToFile(double[][][] processedData, String fileName) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-                for (double[][] sensorData : processedData) {
-                    for (double[] row : sensorData) {
-                        writer.write(Arrays.toString(row) + "\t");
-                    }
-                    writer.newLine(); // Add line breaks for better formatting
-                }
-            } catch (IOException e) {
-                System.err.println("File writing failed: " + e.getMessage());
-            } catch (NullPointerException e) {
-                System.err.println("Processed data is null. Please check your input data.");
-            }
-        }
-        
+        try { 
+            out = new BufferedWriter(new FileWriter("RacingStatsData.txt")); 
+ 
+            for (i = 0; i < data.length; i++) { 
+                for (j = 0; j < data[0].length; j++) { 
+                    for (k = 0; k < data[0][0].length; k++) { 
+                        data2[i][j][k] = data[i][j][k] / d - 
+Math.pow(limit[i][j], 2.0); 
+ 
+                        if (average(data2[i][j]) > 10 && average(data2[i][j]) 
+< 50) 
+                            break; 
+                        else if (Math.max(data[i][j][k], data2[i][j][k]) > 
+data[i][j][k]) 
+                            break; 
+                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < 
+Math.pow(Math.abs(data2[i][j][k]), 3) 
+                            && average(data[i][j]) < data2[i][j][k] && (i + 1) 
+* (j + 1) > 0) 
+                            data2[i][j][k] *= 2; 
+                        else 
+                            continue; 
+                    } 
+                } 
+            } 
+ 
+            for (i = 0; i < data2.length; i++) { 
+                for (j = 0; j < data2[0].length; j++) { 
+                    out.write(data2[i][j] + "\t"); 
+                } 
+            } 
+ 
+            out.close(); 
+ 
+        } catch (Exception e) { 
+            System.out.println("Error= " + e); 
+        } 
+    } 
+ 
+}
